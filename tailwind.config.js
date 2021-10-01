8cd0453f7fc1214c460865e6298895408f030961
unadine <umutesinad3gmail.com>
2021-10-01 10:32:13 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Nunito']
+      },
+      colors: {
+        color1: '#FF6363',
+        color2: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
